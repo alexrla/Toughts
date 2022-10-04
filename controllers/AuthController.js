232feb61e328e86dba31b62e54a94df740899035
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-04 19:56:13 -0300

    Criada a funcionalidade de acessar conta - realizar autenticação

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -10,6 +10,48 @@ module.exports = class AuthController {
 
     }
 
+    static async loginPost(req, res) {
+
+        const { email, password } = req.body;
+
+        // Verificar se o usuário existe e se a senha está correta
+        const user = await User.findOne({ where: { email: email }});
+
+        if(user === null) {
+
+            req.flash("message", "E-mail não encontrado!");
+
+            res.render("auth/login")
+
+            return;
+
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+
+        if(!passwordMatch)  {
+
+            req.flash("message", "Senha incorreta!");
+
+            res.render("auth/login");
+
+            return;
+
+        }
+
+        // Inicializando a sessão
+        req.session.userId = user.id;
+
+        req.flash("message", "Seja bem-vindo!");
+
+        req.session.save(() => {
+
+            res.redirect("/");
+
+        });
+
+    }
+
     static register(req, res) {
 
         res.render("auth/register");
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,8 +5,9 @@ const AuthController = require("../controllers/AuthController");
 const router = express.Router();
 
 router.get("/login", AuthController.login);
+router.post("/login", AuthController.loginPost);
 router.get("/register", AuthController.register);
 router.post("/register", AuthController.registerPost);
-router.get("/logout", AuthController.logout);
+router.get("/logout", AuthController.logout)
 
 module.exports = router;
