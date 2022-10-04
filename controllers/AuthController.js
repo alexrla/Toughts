2858e282ed021bb52bbdbeb0cba6ae7c3e73291e
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-04 19:09:51 -0300

    Finalizada a funcionalidade de criar conta

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -31,6 +31,51 @@ module.exports = class AuthController {
 
         }
 
+        // Verificando se o usuário existe
+        const checkIfUserExists = await User.findOne({ where: { email: email }});
+
+        if(checkIfUserExists)   {
+
+            req.flash("message", "O e-mail já está em uso!");
+
+            res.render("auth/register");
+
+            return;
+
+        }
+
+        // Criando senha
+        const salt = bcrypt.genSaltSync(10);
+
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        };
+
+        try {
+
+            const createrdUser = await User.create(user);
+
+            // Inicializando a sessão
+            req.session.userId = createrdUser.id;
+
+            req.flash("message", "Conta criada com sucesso!");
+
+            req.session.save(() => {
+
+                res.redirect("/");
+
+            });
+            
+        } catch (error) {
+            
+            console.log(error);
+
+        }
+
     }
 
 };
